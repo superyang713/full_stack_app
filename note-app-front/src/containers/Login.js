b5199e52563339c58cb13a8bde9c620ffeacdb42
Yang Dai <yang@Yangs-MacBook-Pro.local>
2019-02-05 23:44:04 -0500

    implement login with cognito

---
+++ note-app-front/src/containers/Login.js
@@ -0,0 +1,69 @@
+import React, { Component } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Auth } from "aws-amplify";
+import "./Login.css";
+
+
+class Login extends Component {
+  state = {
+    email: "",
+    password: ""
+  };
+
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
+
+  handleChange = event => {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
+  handleSubmit = async event => {
+    event.preventDefault();
+    try {
+      await Auth.signIn(this.state.email, this.state.password);
+      alert("Logged in");
+    } catch (e) {
+      alert(e.message);
+    }
+   
+  }
+
+  render() {
+    return (
+      <div className="Login">
+        <form onSubmit={this.handleSubmit}>
+          
+          <FormGroup controlId="email" bsSize="large">
+            <ControlLabel>Email</ControlLabel>
+            <FormControl
+              autoFocus
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+
+          <FormGroup controlId="password" bsSize="large">
+            <ControlLabel>Password</ControlLabel>
+            <FormControl
+              type="password"
+              value={this.state.password}
+              onChange={this.handleChange}
+            />
+          </FormGroup>
+
+          <Button
+            block
+            bsSize="large"
+            disabled={!this.validateForm()}
+            type="submit"
+          >Login</Button>
+          
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
