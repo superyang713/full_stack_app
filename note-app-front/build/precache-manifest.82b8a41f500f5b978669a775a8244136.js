e1bbb501a3b59739a085736cb12f4f6ce1c0e4c1
Yang Dai <yangdai713@aim.com>
2019-02-07 14:04:36 -0500

    v1--first built

---
+++ note-app-front/build/precache-manifest.82b8a41f500f5b978669a775a8244136.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b11b7af3c34dc9f7f5ca",
+    "url": "/static/js/main.b11b7af3.chunk.js"
+  },
+  {
+    "revision": "05f69c2a2fbd9c31dd86",
+    "url": "/static/js/1.05f69c2a.chunk.js"
+  },
+  {
+    "revision": "b11b7af3c34dc9f7f5ca",
+    "url": "/static/css/main.21ea0b7e.chunk.css"
+  },
+  {
+    "revision": "9e36c61781989a96d1200039cf40cb67",
+    "url": "/index.html"
+  }
+];
